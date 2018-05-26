eed5da919d1055dc367b653af0377f893aad08f3
Windows Qiao <342187916@qq.com>
2018-05-26 18:19:17 +0800

    模型类，数据库创建

---
+++ venv_BokE/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\qiao6\Desktop\Pycharm-projects_share\BoKe\venv_BokE\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
